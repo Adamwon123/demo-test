8c585072640bb648f12a02d354e27ac7d31277a3
Adamwon123 <yaesakurazhang@outlook.com>
2026-06-29 21:50:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ class SeatAutoBooker:
             seat = seats[0]
         else:
             seat = random.choice(seats)
-        data = f"beginTime={total_seconds}&duration={3600 * date_config['持续小时数']}&&seats[0]={seat}&seatBookers[0]={self.user_data['uid']}&visitorNum=0"
+        data = f"beginTime={total_seconds}&duration={3600 * date_config['持续小时数']}&&seats[0]={seat}&seatBookers[0]={self.user_data['uid']}"
 
         headers = self.cfg["headers"]
         headers['Cookie'] = self.cookie
